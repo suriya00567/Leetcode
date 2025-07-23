b04da507478384662625c164e95811bca9548dcd
suriya00567 <129683003+suriya00567@users.noreply.github.com>
2025-07-23 15:45:59 +0530

    Create slidingwinMax.py

---
+++ slidingwinMax.py
@@ -0,0 +1,14 @@
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        dq = deque()  
+        result = []
+        for i in range(len(nums)):
+            if dq and dq[0] == i - k:
+                dq.popleft()
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+            dq.append(i)
+            if i >= k - 1:
+                result.append(nums[dq[0]])
+        return result
